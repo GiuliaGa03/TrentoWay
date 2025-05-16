d26a612bd284563758fcdf14e26a8cdd09551b5c
Carlo Oss Cazzador <carlo.osscazzador@studenti.unitn.it>
2025-05-16 09:40:51 +0200

    reso sicuro (non in chiaro) lo scambio della chiave api di google maps

--- app.js
+++ app.js
@@ -27,10 +27,7 @@ app.use(express.urlencoded({ extended: true }));
  */
 app.use(cors())
 
-// Endpoint di prova
-app.get("/api/v1/hello", (req, res) => {
-  res.json({ message: "Ciao, il server è attivo!" });
-});
+
 
 // Serve i file statici dalla cartella "static"
 app.use(express.static(path.join(__dirname, 'static')));
@@ -38,6 +35,13 @@ app.use(express.static(path.join(__dirname, 'static')));
 // Per ogni altro percorso non gestito dai file statici, restituisci `index.html`
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+
+// Questo endpoint restituisce la chiave API di Google Maps in un file JavaScript per via della sicurezza (la chiave deve stare in .env e mai su github)
+app.get('/api/maps-config.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.send(`window.GOOGLE_MAPS_API_KEY = "${process.env.GOOGLE_MAPS_API_KEY}";`);
 });
 
 
--- static/script.js
+++ static/script.js
@@ -1,4 +1,36 @@
-// Funzione che verrà chiamata dal Google Maps API quando carica la mappa
+// utilizzo lo script dinamico config.js dato da questo endpoint "/api/maps-config.js" per caricare la chiave API di Google Maps in modo sicuro
+loadScript('/api/maps-config.js')
+ .then(() => {
+    loadGoogleMapsScript(window.GOOGLE_MAPS_API_KEY);  // Carica lo script di Google Maps
+});
+
+function loadGoogleMapsScript(apiKey) {
+  const script = document.createElement('script');
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+  script.async = true;
+  script.defer = true;
+  document.head.appendChild(script);
+}
+
+
+
+// Funzione di utilità per caricare script esterni
+function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = src;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+}
+
+
+
+
+
+
+// Funzione che verrà chiamata quando carica la mappa
 function initMap() {
   const centerCoords = { lat: 46.0704, lng: 11.1196 };
 
@@ -15,3 +47,4 @@ function initMap() {
     title: "Centro di Trento",
   });
 }
+

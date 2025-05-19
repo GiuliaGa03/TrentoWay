5571fac7a6ee3c26fc00663234bea33f54b29bc7
Carlo Oss Cazzador <carlo.osscazzador@studenti.unitn.it>
2025-05-19 09:58:02 +0200

    aggiustato path file .env

--- tools/seed.js
+++ tools/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 const Segnaposto = require('../models/Segnaposto');
 const Quiz = require('../models/Quiz');

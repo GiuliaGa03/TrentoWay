6d6164f1d0a3f6828cb12d1ce70673cf94d8f27e
Carlo Oss Cazzador <carlo.osscazzador@studenti.unitn.it>
2025-05-28 17:10:33 +0200

    completato middleware

--- autenticazione/middlewareAutenticazione.js
+++ autenticazione/middlewareAutenticazione.js
@@ -8,20 +8,28 @@
 
 const jwt = require('jsonwebtoken');
 
-const authMiddleware = (...allowedRoles) => {
+//commento che spiega il funzionamento del middleware qunado si passa col mouse sopra il nome della funzione
+/**
+ * Middleware di autenticazione + autorizzazione a ruoli specifici
+ * @param {...string} allowedRoles - Ruoli che hanno accesso alla route
+ */
+
+const authMiddleware = (...allowedRoles) => {  //i tre puntini indicano che si possono passare più ruoli come argomenti, prendendoli come un array
   return (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    const authHeader = req.headers.authorization; // recupera l'header di autorizzazione dalla richiesta
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {    // controlla se l'header esiste e inizia con 'Bearer '
       return res.status(401).json({ message: 'Token mancante o malformato' });
     }
 
-    const token = authHeader.split(' ')[1];
+    // essento che l'eader è composto da ' Bearer "token" ' seguito dal token, per prendere il token si divide la stringa
+    // tra Bearrer e token dove c'è lo spazio e si prende la seconda parte [1], ovvero solo il token
+    const token = authHeader.split(' ')[1]; 
 
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = decoded; // es: { userId: "...", role: "admin" }
-
-      if (allowedRoles.length > 0 && !allowedRoles.includes(decoded.role)) {
+        //la prima condizione controlla se sono stati passati dei ruoli e la seconda se il ruolo dell'utente non è tra quelli consentiti
+      if (allowedRoles.length > 0 && !allowedRoles.includes(decoded.role)) { 
         return res.status(403).json({ message: 'Accesso negato: ruolo insufficiente' });
       }
 

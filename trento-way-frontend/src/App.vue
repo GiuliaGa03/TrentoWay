dff526f15afbf2bc9d401b611e196cb6eb63f339
GiuliaGaronzi <giuliagaronzi03@gmail.com>
2025-06-02 16:18:27 +0200

    installazione tailwind

---
+++ trento-way-frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen bg-blue-600 text-white p-8 text-center">
+    🎉 Tailwind è attivo in Vue!
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
